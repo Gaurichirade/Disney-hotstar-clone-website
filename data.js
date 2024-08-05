4a50b5f7bf942b95996d2a8d5918bdacc4e2c0ba
Gaurichirade <149358268+Gaurichirade@users.noreply.github.com>
2024-08-05 12:33:27 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Sita Ramam',
+        des: 'On a letter-delivering task, Afreen learns of a 20-year-old romance set amidst a war. Her quest to trace the lovers makes her embrace humanity beyond borders.',
+        image: 'Shows/slide/sitaramam slide.jpeg'
+    },
+    {
+        name: 'Avengers Endgame',
+        des: 'After the devastating events of the infinity war, the Avengers assemble once more to reverse actions by thanos and restore balance to the universe',
+        image: 'Shows/slide/avengers slide.png'
+    },
+    {
+        name: 'tangled',
+        des: 'The most wanted bandit Flynn Rider is taken hostage by Rapunzel,a teen with 70 feet of hair, and the unlikely duo sets off on a hillarious adventure.',
+        image: 'Shows/slide/tangled slide.png'
+    },
+    {
+        name: 'Aashiqana',
+        des: 'A serial killer sparks the story of upright Yash and feisty Chikki. plagued by misunderstandings, how far do they have to go to nab the murderer?',
+        image: 'Shows/slide/aashiqana slide.jpeg'
+    },
+    {
+        name: 'Pop Kaun?',
+        des: 'Name-dropping his politician dad, Sahil lives life king-size. Then he learns he is adopted! And begins his quest to find an answer to - Mera Baap Kaum Hai?',
+        image: 'Shows/slide/pk slide2.jpeg'
+    }
+]
